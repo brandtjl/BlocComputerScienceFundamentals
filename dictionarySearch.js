b312d0f7a04e2d2b5dc6469c99324714d7d15a4b
Jeni Brandt <jenibrandt@legalshieldcorp.com>
2018-05-22 09:10:29 -0500

    Modified dictionary search

--- dictionarySearch.js
+++ dictionarySearch.js
@@ -1,9 +1,18 @@
-var linearSearch = function(collection, target) {
-    var result
-    for(var i = 0; i < collection.length; i++) {
-      if(collection[i] === target){
-        result = i;
-      }
+var binarySearch = function(collection, value) {
+  var low = 0;
+  var high = collection.length - 1;
+  var mid
+  while (low <= high) {
+    mid = Math.floor((low + high) / 2)
+    console.log(collection[low] + ' ' + collection[high])
+    if (collection[mid] > value) {
+      console.log(mid + ' ' + value)
+      high = mid -1
+    } else if (collection[mid] < value) {
+      low = mid + 1
+    } else {
+      return mid
     }
-    return result;
-  };
+  } 
+  return 'Not Found'
+}
